aeb31df5f98027c99b3f5cda7121e6119602c8e2
wecodeRUS <monestro99@mail.ru>
2026-01-28 16:47:46 +0300

    x

--- api/_config.php
+++
@@ -1,2 +0,0 @@
-<?php
-$PUSH_KEY='kRp_92384_x';
--- api/economy.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__.DIRECTORY_SEPARATOR.'_utils.php';
-$p=_push_get('economy', 31536000);
-if(is_array($p)) _json($p);
-_json(array('ok'=>false,'players'=>array(),'top3'=>array(),'updated_at'=>time()));
